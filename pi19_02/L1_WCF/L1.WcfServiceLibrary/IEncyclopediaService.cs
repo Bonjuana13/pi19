5a6a2414e03885999e836e0508e8d773c125d9f4
Bonjuana Volt <72211633+Bonjuana13@users.noreply.github.com>
2021-03-13 09:53:15 +0700

    the first commit.

--- pi19_02/L1_WCF/L1.WcfServiceLibrary/IEncyclopediaService.cs
+++ pi19_02/L1_WCF/L1.WcfServiceLibrary/IEncyclopediaService.cs
@@ -7,93 +7,93 @@ using System.Text;
 
 namespace L1.WcfServiceLibrary
 {
-  /// <summary>
-  /// Сервис "Электронная энциклопедия"
-  /// </summary>
-  [ServiceContract]
-  public interface IEncyclopediaService
-  {
     /// <summary>
-    /// Получение списка категорий и информации о энциклопедии
+    /// Сервис "Электронная энциклопедия"
     /// </summary>
-    /// <returns></returns>
-    [OperationContract]
-    EncyclopediaType GetInfo();
+    [ServiceContract]
+    public interface IEncyclopediaService
+    {
+        /// <summary>
+        /// Получение списка категорий и информации о энциклопедии
+        /// </summary>
+        /// <returns></returns>
+        [OperationContract]
+        EncyclopediaType GetInfo();
 
-    /// <summary>
-    /// Получить информацию по разделу энциклопедии
-    /// </summary>
-    /// <param name="sCode"></param>
-    /// <returns></returns>
-    [OperationContract]
-    EncyclopediaPartType GetPart(string sCode);
+        /// <summary>
+        /// Получить информацию по разделу энциклопедии
+        /// </summary>
+        /// <param name="sCode"></param>
+        /// <returns></returns>
+        [OperationContract]
+        EncyclopediaPartType GetPart(string sCode);
 
-    /// <summary>
-    /// Получить полную словарную статью
-    /// </summary>
-    /// <param name="sPart"></param>
-    /// <param name="sCode"></param>
-    /// <returns></returns>
-    [OperationContract]
-    EncyclopediaArticleType GetArticle(string sPart, string sCode);
-  }
-
-  /// <summary>
-  /// Энциклопедия
-  /// </summary>
-  [DataContract]
-  public class EncyclopediaType
-  {
-    /// <summary>
-    /// Название энциклопедии
-    /// </summary>
-    [DataMember]
-    public string Title { get; set; }
+        /// <summary>
+        /// Получить полную словарную статью
+        /// </summary>
+        /// <param name="sPart"></param>
+        /// <param name="sCode"></param>
+        /// <returns></returns>
+        [OperationContract]
+        EncyclopediaArticleType GetArticle(string sPart, string sCode);
+    }
 
     /// <summary>
-    /// Список разделов энциклопедии
+    /// Энциклопедия
     /// </summary>
-    [DataMember]
-    public EncyclopediaPartType[] PartList { get; set; }
+    [DataContract]
+    public class EncyclopediaType
+    {
+        /// <summary>
+        /// Название энциклопедии
+        /// </summary>
+        [DataMember]
+        public string Title { get; set; }
 
-    // TODO
-  }
+        /// <summary>
+        /// Список разделов энциклопедии
+        /// </summary>
+        [DataMember]
+        public EncyclopediaPartType[] PartList { get; set; }
 
-  /// <summary>
-  /// Раздел энциклопедии
-  /// </summary>
-  [DataContract]
-  public class EncyclopediaPartType
-  {
+        // TODO
+    }
+
     /// <summary>
-    /// Список разделов энциклопедии
+    /// Раздел энциклопедии
     /// </summary>
-    [DataMember]
-    public EncyclopediaArticleInfoType[] ArticleInfoList { get; set; }
+    [DataContract]
+    public class EncyclopediaPartType
+    {
+        /// <summary>
+        /// Список разделов энциклопедии
+        /// </summary>
+        [DataMember]
+        public EncyclopediaArticleInfoType[] ArticleInfoList { get; set; }
+
+        /// <summary>
+        /// Имя папки
+        /// </summary>
+        [DataMember]
+        public string Folder { get; set; }
+        // TODO
+    }
 
     /// <summary>
-    /// Имя папки
+    /// Краткая информация о статье энциклопедии
     /// </summary>
-    [DataMember]
-    public string Folder { get; set; }
-    // TODO
-  }
+    [DataContract]
+    public class EncyclopediaArticleInfoType
+    {
+        // TODO
+    }
 
-  /// <summary>
-  /// Краткая информация о статье энциклопедии
-  /// </summary>
-  [DataContract]
-  public class EncyclopediaArticleInfoType
-  {
-    // TODO
-  }
-
-  /// <summary>
-  /// Полная статья энциклопедии с иллюстрацией
-  /// </summary>
-  [DataContract]
-  public class EncyclopediaArticleType
-  {
-    // TODO
-  }
+    /// <summary>
+    /// Полная статья энциклопедии с иллюстрацией
+    /// </summary>
+    [DataContract]
+    public class EncyclopediaArticleType
+    {
+        // TODO
+    }
 }
